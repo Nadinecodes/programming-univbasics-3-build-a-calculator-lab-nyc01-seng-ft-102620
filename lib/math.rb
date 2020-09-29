bd435fe33f740f553313ea5b0be4a56e4b9c978b
Nadine Edwards <edwardsnadine1985@gmail.com>
2020-09-29 16:16:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,31 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  subtract=num1-num2
+  return subtract
+  subtract
 end
 
 def division(num1, num2)
-
+  divide=num1/num2
+  return divide
+  divide
 end
 
 def multiplication(num1, num2)
-
+  multiply=num1*num2
+  return multiply
+  multiply
 end
 
 def modulo(num1, num2)
-
+  mod=num1%num2
+  return mod
+  mod
 end
 
 def square_root(num)
-
+  sr=Math.sqrt(num1, num2)
+  return sr
+  sr
 end
